019bc5c5b7962ac33c2864e55a702f27d2a598b2
Jim Callahan <jcallahan@theladders.com>
2017-03-25 17:15:19 -0400

    positions now properly advanced by moves

--- src/cljs/maverick/events.cljs
+++ src/cljs/maverick/events.cljs
@@ -45,21 +45,34 @@
          now (.now js/Date)]
      (if start-location
        (if (= loc start-location)
+         
          ;; Cancel the move.
          (update-in db [::db/current-move] dissoc ::db/start-location)
+
          ;; Make a move.
          (let [move (assoc pmove
                            ::db/end-location loc
                            ::db/end-stamp now)
-               next (-> (case color
-                          ::db/white {::db/move-number move-number
-                                      ::db/color ::db/black}
-                          ::db/black {::db/move-number (inc move-number)
-                                      ::db/color ::db/white})
-                        (assoc ::db/start-stamp now))]
+               next-move (-> (case color
+                               ::db/white {::db/move-number move-number
+                                           ::db/color ::db/black}
+                               ::db/black {::db/move-number (inc move-number)
+                                           ::db/color ::db/white})
+                             (assoc ::db/start-stamp now))
+               plocs (-> db ::db/current-position ::db/locations)
+               sloc (::db/start-location move)
+               piece (get plocs sloc)
+               end-locations {::db/locations (-> plocs
+                                                 (dissoc sloc)
+                                                 (assoc loc piece))}
+               position (-> (select-keys pmove [::db/move-number ::db/color])
+                            (merge end-locations))]
            (-> db
                (update-in [::db/moves] conj move)
-               (assoc ::db/current-move next))))
+               (assoc ::db/current-move next-move)
+               (update-in [::db/positions] conj position)
+               (assoc ::db/current-position end-locations))))
+
        ;; Start a move.
        (if (rules/can-move? (cur-rules db) db loc)
          (let [kind (-> db
